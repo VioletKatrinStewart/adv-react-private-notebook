ad19b465c8d685de918adaacf2d6215b09de4943
Violet Stewart <vkazoo2@gmail.com>
2022-02-23 14:49:30 -0800

    added auth but page wont render

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,18 +1,17 @@
-// A wrapper for <Route> that redirects to the login
-// screen if you're not yet authenticated.
-// export default function PrivateRoute({ children, ...rest }) {
-  // TODO: Use the user in context to determine whether to redirect to /login
-
 import React from 'react';
 import { Redirect, Route, useLocation } from 'react-router-dom';
-import { useUser } from '../../Context/UserContext';
+import { useUser } from '../../context/UserContext';
 
 export default function PrivateRoute({ children, ...routeProps }) {
   const { user } = useUser();
   const location = useLocation();
   return (
     <Route {...routeProps}>
-      {user ? children : <Redirect to={{ pathname: '/login', state: { from: location } }} />}
+      {user ? (
+        children
+      ) : (
+        <Redirect to={{ pathname: '/login', state: { from: location } }} />
+      )}
     </Route>
   );
 }
